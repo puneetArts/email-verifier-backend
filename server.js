ac0d73eb5f71fa5526070a354f7472a8e4ec0764
Puneet Singh <puneetsingh91100@gmail.com>
2025-12-30 20:03:58 +0530

    vercel-commit-2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://email-frontend-swart.vercel.app/"
+      "https://email-frontend-ec62.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
